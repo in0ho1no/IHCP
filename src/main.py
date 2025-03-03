e63fb59ea793b147796a3731b8812451c8aaf9bc
in0ho1no <yin0ho1no@gmail.com>
2025-03-03 20:36:22 +0900

    入力ファイルに対応したファイル出力に対応

--- src/main.py
+++ src/main.py
@@ -1,25 +1,27 @@
 import glob
+import os
 
 from parse import SimpleDiagramParser
 from render import SVGRenderer
 
 
 def main() -> None:
-    # 入力テキスト ファイルから読み込む際もrawデータで読むこと
     # HCPファイル読み込み
     hcp_file_list = glob.glob(r"./src/input/*.hcp")
     for hcp_file in hcp_file_list:
         with open(hcp_file, encoding="utf-8") as f:
             input_text = f.read()
 
-        # パースと描画
+        # パース
         parser = SimpleDiagramParser(input_text)
 
+        # 描画
         renderer = SVGRenderer(parser.process_line_info_list, parser.data_line_info_list)
         svg_output = renderer.render()
 
         # SVGファイルとして保存
-        with open(r"./src/output/output.svg", "w", encoding="utf-8") as f:
+        basename = os.path.basename(hcp_file).split(".")[0]
+        with open(f"./src/output/{basename}.svg", "w", encoding="utf-8") as f:
             f.write(svg_output)
 
 
